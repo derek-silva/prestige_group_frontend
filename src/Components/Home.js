12c86cf77fabf2bcde33c7e93d4e58ceb50ee8b4
github username <github email address>
2018-09-29 12:55:49 -0500

    added button to sign up

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,9 +5,29 @@ export default () => {
   return (
     <div>
       <Parallax imageSrc={require("../images/more_buildings.jpg")} />
+      <div className="center row container">
+        <h2 className="header">Join the Future of Investing</h2>
+        <p className="grey-text text-darken-3 lighten-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu,
+          non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas
+          eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem
+          neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam
+          eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla
+          imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac,
+          gravida ultrices erat. Nullam eget dignissim mauris, non tristique
+          erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia Curae
+        </p>
+        <Button waves="light" className="teal darken-4">
+          Become an Investor
+        </Button>
+      </div>
+
+      <Parallax imageSrc={require("../images/blur.jpg")} />
       <div className="center section white">
         <div className="row container">
-          <h2 className="header">Our Mission</h2>
+          <h2 className="header">About us</h2>
           <p className="grey-text text-darken-3 lighten-3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
             scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu,
@@ -22,53 +42,26 @@ export default () => {
           </p>
         </div>
       </div>
-      <Parallax imageSrc={require("../images/blur.jpg")} />
-      <div className="center row container">
-        <h2 className="header">Become an Investor</h2>
-        <p className="grey-text text-darken-3 lighten-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu,
-          non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas
-          eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem
-          neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam
-          eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla
-          imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac,
-          gravida ultrices erat. Nullam eget dignissim mauris, non tristique
-          erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia Curae
-        </p>
-      </div>
+
       <Parallax imageSrc={require("../images/gold.jpg")} />
       <footer class="page-footer teal darken-4">
         <div class="container">
           <div class="row">
             <div class="col l6 s12">
-              <h5 class="white-text">Company Bio</h5>
+              <h5 class="white-text">Prestige Group LP</h5>
               <p class="grey-text text-lighten-4">
-                We are a team of college students working on this project like
-                it's our full time job. Any amount would help support and
-                continue development on this project and is greatly appreciated.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                scelerisque id nunc nec volutpat. Etiam pellentesque tristique
+                arcu, non consequat magna fermentum ac. Cras ut ultricies eros.
+                Maecenas eros justo, ullamcorper a sapien id, viverra ultrices
+                eros. Morbi sem neque, posuere et pretium eget, bibendum
+                sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu
+                mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi
+                massa odio, condimentum sed ipsum ac, gravida ultrices erat.
+                Nullam eget dignissim mauris, non tristique erat. Vestibulum
+                ante ipsum primis in faucibus orci luctus et ultrices posuere
+                cubilia Curae
               </p>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Settings</h5>
-              <ul>
-                <li>
-                  <a class="white-text" href="#!">
-                    Link 1
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Connect</h5>
-              <ul>
-                <li>
-                  <a class="white-text" href="#!">
-                    Link 1
-                  </a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
---
+++ src/Components/SignUp.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class LogIn extends Component {
+  render() {
+    return (
+      <div>
+        <h1 align="center">Sign Up</h1>
+        <div
+          style={{
+            width: "600px",
+            margin: "auto"
+          }}
+          className="form-container"
+        >
+          <form>
+            <div>
+              <div className="input-field col s6">
+                <input id="email" type="text" className="validate" />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s5">
+                <input id="password" type="password" className="validate" />
+                <label htmlFor="password">Password</label>
+              </div>
+              <div className="center">
+                <button type="submit" className="btn teal darken-4 hoverable">
+                  Log In
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}

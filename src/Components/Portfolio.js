bebecc24f9935c08a8ba251b21586d167674ecee
github username <github email address>
2018-10-01 12:28:46 -0500

    portfolio start

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 
 export default class Portfolio extends Component {
+  constructor() {
+    super();
+    this.state = {
+      holdings: []
+    };
+  }
+
+  fetchHoldings() {
+    fetch("http://localhost:3001/holdings")
+      .then(holdingsData => holdingsData.json())
+      .then(h => this.setState({ holdings: h }));
+  }
+
+  componentDidMount() {
+    this.fetchHoldings();
+  }
+
   render() {
     return (
       <div>

18b81f0c71df14c2ed9e6424a1ca0b384fb7fe91
Sean Huang <hyprul@ucla.edu>
2018-10-03 16:19:18 -0500

    fixed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -13,7 +13,7 @@ export default () => {
       <h1 className="header teal-text text-darken-4">Contact Us</h1>
       <hr />
       <div class="row">
-        <div class="col s12 m4">
+        <div class="col s12 m6">
           <div className="card hoverable">
             <div className="card-header teal darken-4">
               <h5>Sean "The Don" Huang</h5>
@@ -44,38 +44,7 @@ export default () => {
           </div>
         </div>
 
-        <div class="col s12 m4">
-          <div className="card hoverable">
-            <div className="card-header teal darken-4">
-              <h5>Nancy Do</h5>
-            </div>
-            <div>&nbsp;</div>
-            <img
-              style={{
-                width: "300px",
-                margin: "auto"
-              }}
-              src="https://avatars2.githubusercontent.com/u/39013174?s=400&v=4"
-              alt=""
-            />
-            <ul>
-              <li>email: nancydo@prestigegroup.com</li>
-              <li>phone: 555-555-5555</li>
-              <li>fax: 444-444-4444</li>
-            </ul>
-            <div className="card-action">
-              <a
-                href="https://github.com/bunnyd"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col s12 m4">
+        <div class="col s12 m6">
           <div className="card hoverable">
             <div className="card-header teal darken-4">
               <h5>Derek Silva</h5>
